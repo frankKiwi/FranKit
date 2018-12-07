bdc9b2fd85dc97565527943d6222aa32ee6624ed
fanrenFRank <1778907544@qq.com>
2018-12-07 11:27:22 +0800

    修改

--- FranKit/FranKitTool.h
+++ FranKit/FranKitTool.h
@@ -13,9 +13,23 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FranKitTool : NSObject
 /**
+ * @Frank 图片下载的时候不解压 降低内存消耗 在APPdelegate里面注册
+ */
+/**
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application{
+NSLog(@"@Frank内存警告了⚠️⚠️⚠️⚠️⚠️⚠️⚠️");
+SDWebImageManager *mgr = [SDWebImageManager sharedManager];
+[mgr cancelAll];
+[mgr.imageCache clearMemory];
+}
+**/
+/**
  * @Frank 图片下载的时候不解压 降低内存消耗
  */
 +(void)regisinDepressForSd;
+/**
+ * @Frank 图片下载的时候不解压 降低内存消耗 还原配置
+ */
 +(void)resetDepressForSd;
 @end
 
